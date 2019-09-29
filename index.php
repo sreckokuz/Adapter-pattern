75abd86c2fe46014bf62d87b6f8635ed8d9af190
Sreckokuz <sreckokuz@yahoo.com>
2019-09-29 21:46:58 +0200

    task description

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ use App\Person;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Imamo klasu Person koja ima implementirane dvije funkcije kojima se omogucava citanje knjige: open(), read().
+// Medjutim, postoje i elektronske knjige, koje takodje zelimo citati, ali mi zelimo takodje da zadrzimo iste funkcije open() i read().
+
+
 $person = new Person();
 $book = new Book();
 $kindle = new Kindle();
